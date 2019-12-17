980730f20ef00092be7151fe0108b8d4b2b43231
Karl <kc0bfv@gmail.com>
2019-12-17 13:50:56 -0700

    Day 17 without cheating

--- 2019/17/day_17.rs
+++ 2019/17/day_17.rs
@@ -70,16 +70,22 @@ struct ASCII {
     robot: Position,
     cmd_pos: usize,
     map_lines: usize,
+    wait_for_inputs: bool,
+    cmds: String,
+    robot_dir: Position,
 }
 impl ASCII {
     fn new() -> Self {
         Self {
             view: vec![vec![]],
             scaf: vec![],
-            moves: vec!["R".to_string()], // cheating
+            moves: vec![],
             robot: Position::new(0, 0),
             cmd_pos: 0,
             map_lines: 0,
+            wait_for_inputs: false,
+            cmds: String::new(),
+            robot_dir: Position::new(0,0),
         }
     }
 
@@ -97,7 +103,6 @@ impl ASCII {
             }
             println!("");
         }
-        println!("\n\n\n");
         self.map_lines = self.view.len();
     }
 
@@ -132,6 +137,8 @@ impl ASCII {
                 if surr_scaf.len() != 1 { panic!("Wrong surr scaff in init!"); }
                 surr_scaf.remove(0)
             };
+        // Add the initial turn
+        self.moves.push(self.robot_dir.turn_lookup(&cur_dir).to_string());
         loop {
             // Add each step to the scaffold vector
             self.scaf.push(cur_pos.clone());
@@ -192,21 +199,147 @@ impl ASCII {
     fn calc_alignment(&self) -> i64 {
         self.find_dupe_scaf().iter().map(|pos| pos.x * pos.y).sum()
     }
+
+    fn reset_for_part_2(&mut self) {
+        self.wait_for_inputs = true;
+    }
+
+    fn fit_moves(&mut self) {
+        let mut move_pairs: Vec<String> = vec![];
+        for ind in 0..(self.moves.len()/2) {
+            let outstr: String = self.moves[2*ind]
+                    .chars()
+                    .chain(self.moves[2*ind+1].chars())
+                    .collect();
+            move_pairs.push(outstr);
+        }
+        //println!("pairs {:?}", move_pairs);
+
+        let a_st = 0;
+        let (mut b_st, mut c_st): (usize, usize);
+        for a_ed in (a_st + 1)..move_pairs.len() {
+            let a_str: String = self.moves[(a_st*2)..((a_ed+1)*2)].join(",");
+            if a_str.len() > 20 {
+                println!("FAILED!");
+                break;
+            }
+            // Look for repeats of a before starting b
+            b_st = a_ed + 1;
+            let mut sub_pat_a: Vec<&str> = vec!["A"];
+            let mut test_pos = a_ed + 1;
+            while test_pos < move_pairs.len() && (move_pairs[test_pos] == move_pairs[test_pos - b_st + a_st]) {
+                test_pos += 1;
+                // If we're at a multiple of a's size, reset a
+                if (test_pos - b_st) % (a_ed + 1 - a_st) == 0 {
+                    b_st = test_pos;
+                    sub_pat_a.push("A");
+                }
+            }
+            
+            for b_ed in (b_st + 1)..move_pairs.len() {
+                let b_str: String = self.moves[(b_st*2)..((b_ed+1)*2)].join(",");
+                if b_str.len() > 20 {
+                    break;
+                }
+                
+                // Look for repeats of a or b before starting c
+                c_st = b_ed + 1;
+                let mut sub_pat_b: Vec<&str> = sub_pat_a.iter().cloned().collect();
+                sub_pat_b.push("B");
+                let mut test_pos = b_ed + 1;
+                let mut found_match = true;
+                while found_match {
+                    found_match = false;
+                    while test_pos < move_pairs.len() && (move_pairs[test_pos] == move_pairs[test_pos - c_st + a_st]) {
+                        test_pos += 1;
+                        // If we're at a multiple of a's size, reset a
+                        if (test_pos - c_st) % (a_ed + 1 - a_st) == 0 {
+                            c_st = test_pos;
+                            found_match = true;
+                            sub_pat_b.push("A");
+                        }
+                    }
+                    while test_pos < move_pairs.len() && (move_pairs[test_pos] == move_pairs[test_pos - c_st + b_st]) {
+                        test_pos += 1;
+                        // If we're at a multiple of a's size, reset a
+                        if (test_pos - c_st) % (b_ed + 1 - b_st) == 0 {
+                            c_st = test_pos;
+                            found_match = true;
+                            sub_pat_b.push("B");
+                        }
+                    }
+                }
+
+                for c_ed in (c_st + 1)..move_pairs.len() {
+                    let c_str: String = self.moves[(c_st*2)..((c_ed+1)*2)].join(",");
+                    if c_str.len() > 20 {
+                        break;
+                    }
+
+                    let mut fin = c_ed + 1;
+                    let mut sub_pat_c: Vec<&str> = sub_pat_b.iter().cloned().collect();
+                    sub_pat_c.push("C");
+                    let mut test_pos = c_ed + 1;
+                    let mut found_match = true;
+                    while found_match {
+                        found_match = false;
+                        while test_pos < move_pairs.len() && (move_pairs[test_pos] == move_pairs[test_pos - fin + a_st]) {
+                            test_pos += 1;
+                            // If we're at a multiple of a's size, reset
+                            if (test_pos - fin) % (a_ed + 1 - a_st) == 0 {
+                                fin = test_pos;
+                                found_match = true;
+                                sub_pat_c.push("A");
+                            }
+                        }
+                        while test_pos < move_pairs.len() && (move_pairs[test_pos] == move_pairs[test_pos - fin + b_st]) {
+                            test_pos += 1;
+                            // If we're at a multiple of b's size, reset
+                            if (test_pos - fin) % (b_ed + 1 - b_st) == 0 {
+                                fin = test_pos;
+                                found_match = true;
+                                sub_pat_c.push("B");
+                            }
+                        }
+                        while test_pos < move_pairs.len() && (move_pairs[test_pos] == move_pairs[test_pos - fin + c_st]) {
+                            test_pos += 1;
+                            // If we're at a multiple of c's size, reset
+                            if (test_pos - fin) % (c_ed + 1 - c_st) == 0 {
+                                fin = test_pos;
+                                found_match = true;
+                                sub_pat_c.push("C");
+                            }
+                        }
+                    }
+
+                    if test_pos == move_pairs.len() {
+                        let cmd_str: String = sub_pat_c.join(",");
+                        self.cmds = format!("{}\n{}\n{}\n{}\n", cmd_str, a_str, b_str, c_str);
+                        //println!("Found: {}", self.cmds);
+                        return ();
+                    }
+                }
+            }
+        }
+    }
 }
 impl Iterator for ASCII {
     type Item = i64;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let cmds = "C,A,C,A,B,C,A,B,C,B\nL,8,L,6,L,10,L,6\nR,6,L,8,L,10,R,6\nR,6,L,6,L,10\n";
+        //let cmds = "C,A,C,A,B,C,A,B,C,B\nL,8,L,6,L,10,L,6\nR,6,L,8,L,10,R,6\nR,6,L,6,L,10\n";
+        //println!("{}\n{}", cmds, self.cmds);
+        let cmds = self.cmds.clone();
         let retval: Self::Item;
 
-        println!("Sending program! {} {}", self.cmd_pos, cmds.len());
+        //println!("Sending program! {} {}", self.cmd_pos, cmds.len());
         if self.cmd_pos < cmds.len() {
             retval = cmds.bytes().skip(self.cmd_pos).next().unwrap() as i64;
         } else if self.cmd_pos == cmds.len() {
             retval = 'y' as i64;
         } else if self.cmd_pos == cmds.len() +1 {
             retval = '\n' as i64;
+            self.wait_for_inputs = false;
         } else {
             panic!("Another input requested!");
         }
@@ -223,6 +356,11 @@ impl Iterator for ASCII {
 }
 impl OutputHandler for ASCII {
     fn output(&mut self, val: i64) -> Result<(), &'static str> {
+        // Handle the ignored lines at the beginning of part 2
+        if self.wait_for_inputs {
+            return Ok(());
+        }
+
         if self.map_lines > 0 && self.view.len() == self.map_lines {
             self.view = vec![];
             self.view.push(vec![]);
@@ -234,9 +372,10 @@ impl OutputHandler for ASCII {
 
         let last_line = self.view.len() - 1;
         let val_as_char = (val as u8) as char;
+
         // Store view
-        match val {
-            10 => self.view.push(Vec::new()),
+        match val_as_char {
+            '\n' => if self.view[last_line].len() > 0 { self.view.push(Vec::new()) },
             _ => self.view[last_line].push(val_as_char),
         }
         // Find robot
@@ -244,6 +383,14 @@ impl OutputHandler for ASCII {
             '^' | 'v' | '<' | '>' | 'X' => self.robot =
                     Position::new((self.view[last_line].len() - 1) as i64,
                             (self.view.len() - 1) as i64),
+            _ => (),
+        }
+        // Get robot dir
+        match val_as_char {
+            '^' => self.robot_dir = Position::new( 0, -1),
+            'v' => self.robot_dir = Position::new( 0,  1),
+            '<' => self.robot_dir = Position::new(-1,  0),
+            '>' => self.robot_dir = Position::new( 1,  0),
             _ => (),
         }
         if self.map_lines > 0 && self.view.len() == self.map_lines {
@@ -261,10 +408,12 @@ fn main() {
     run_program(0, &input_prog, &mut ascii);
     ascii.print_map(None);
     ascii.interpret_map();
+    ascii.fit_moves();
     println!("Part 1: {}", ascii.calc_alignment());
-    println!("Moves: {:?}", ascii.moves);
+    //println!("Moves: {:?}", ascii.moves);
 
     let mut part_2_input: Vec<i64> = vec![2];
     part_2_input.extend(input_prog[1..].iter().cloned());
+    ascii.reset_for_part_2();
     run_program(0, &part_2_input, &mut ascii);
 }
