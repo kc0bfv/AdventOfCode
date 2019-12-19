b2602786b504af5514df2574b6116c24f6e12e91
Karl <kc0bfv@gmail.com>
2019-12-19 16:00:50 -0700

    Simpler part 2 solution

--- 2019/19/day_19.rs
+++ 2019/19/day_19.rs
@@ -147,107 +147,68 @@ impl Drone {
         // Follow the left and right edges until they are further than 100 points apart
         // Edges will be the left/right-most tractor beam position at a y
         // Track these positions separately from next part
-        if self.p2_edge_mode {
-            // Find right edge mode
-            if self.p2_edge_find_right {
-                // Are we just starting in this mode?
-                if self.p2_edge_find_just_start {
-                    self.p2_edge_find_just_start = false;
-
-                    let pos_below_last_re = Position::new(self.last_re.x,
-                            self.last_re.y + 1);
-                    return pos_below_last_re;
+        while self.p2_edge_mode {
+            let checking_y = self.last_le.y + 1;
+            let mut checking_le_x = self.last_le.x;
+            let mut checking_re_x = self.last_re.x;
+        
+            loop {
+                let le_check = Position::new(checking_le_x, checking_y);
+                if !self.is_explored(&le_check) {
+                    return le_check;
+                } else if self.is_trac(&le_check) {
+                    break;
                 }
-
-                // Did we run off the tractor beam edge thus finding it?
-                else if !self.is_trac(&self.last_move) {
-                    // Save the right edge
-                    self.last_re = Position::new(self.last_move.x - 1,
-                            self.last_move.y);
-                    // update state and re-run the calculator to find left edge
-                    self.p2_edge_find_right = false;
-                    self.p2_edge_find_just_start = true;
-                    return self.calc_next_part_2_edge();
-                }
-
-                // Or are we still on a tractor beam part and thus looking for the edge
-                else {
-                    let next_x = self.last_move.x + 1;
-                    return Position::new(next_x, self.last_move.y);
-                }
+                checking_le_x += 1;
             }
-            // Find left edge mode
-            else {
-                // Are we just starting in this mode?
-                if self.p2_edge_find_just_start {
-                    self.p2_edge_find_just_start = false;
-
-                    let pos_below_last_le = Position::new(self.last_le.x,
-                            self.last_le.y + 1);
-                    return pos_below_last_le;
+            loop {
+                let re_check = Position::new(checking_re_x, checking_y);
+                if !self.is_explored(&re_check) {
+                    return re_check;
+                } else if !self.is_trac(&re_check) {
+                    checking_re_x -= 1;
+                    break;
                 }
+                checking_re_x += 1;
+            }
+            self.last_le = Position::new(checking_le_x, checking_y);
+            self.last_re = Position::new(checking_re_x, checking_y);
 
-                // Did we find a trac beam thus finding the edge?
-                else if self.is_trac(&self.last_move) {
-                    // Save the left edge
-                    self.last_le = Position::new(self.last_move.x,
-                            self.last_move.y);
-
-                    // Did we find a spot that's wide enough?
-                    if (self.last_re.x - self.last_le.x) + 1 >= box_size {
-                        // Allow moving into length finding mode
-                        self.p2_edge_mode = false;
-
-                        //println!("Found 100 width area {:?} {:?} {}", self.last_le, self.last_re, self.last_re.x - self.last_le.x);
-                    } else {
-                        // Stay in edge finding mode
-                        self.p2_edge_mode = true;
-                    }
-
-                    // Reset the edge finding mode state
-                    self.p2_edge_find_right = true;
-                    self.p2_edge_find_just_start = true;
-
-                    // Rerun the calculator
-                    return self.calc_next_part_2_edge();
-                }
-
-                // Or are we off a tractor beam and thus looking for the edge
-                else {
-                    let next_x = self.last_move.x + 1;
-                    return Position::new(next_x, self.last_move.y);
-                }
+            if (self.last_re.x - self.last_le.x) >= (box_size - 1) {
+                self.p2_edge_mode = false;
             }
         }
 
         // Length finding mode
-        // When edge finding mode worked, now track the left edge to make sure it stays >= 100 points apart from the old right edge's x for 100 points
+        // When edge finding mode worked, now track the left edge to make 
+        // sure it stays >= 100 points apart from the old right edge's x for
+        // 100 points
+
+        // Check the bottom left corner of the box
+        // If it's in the tractor beam, and the top right is (it's self.last_re)
+        // then the whole thing is
+        let x_tst = self.last_re.x - (box_size - 1);
+        let y_tst = self.last_le.y + (box_size - 1);
+        let test_pos = Position::new(x_tst, y_tst);
+
+        // If we don't know what's in that corner, explore it
+        if !self.is_explored(&test_pos) {
+            return test_pos;
+        }
+        // If bottom left corner isn't track, go back to edge finding
+        if !self.is_trac(&test_pos) {
+            // Jump to edge finding mode
+            self.p2_edge_mode = true;
+
+            // Rerun the calculator
+            return self.calc_next_part_2_edge();
+        }
+        // If bottom left corner is track, we're done.
         else {
-            // Check the bottom left corner of the box
-            // If it's in the tractor beam, and the top right is (it's self.last_re)
-            // then the whole thing is
-            let x_tst = self.last_re.x - (box_size - 1);
-            let y_tst = self.last_le.y + (box_size - 1);
-            let test_pos = Position::new(x_tst, y_tst);
-
-            // If we don't know what's in that corner, explore it
-            if !self.is_explored(&test_pos) {
-                return test_pos;
-            }
-            // If bottom left corner isn't track, go back to edge finding
-            if !self.is_trac(&test_pos) {
-                // Jump to edge finding mode
-                self.p2_edge_mode = true;
-
-                // Rerun the calculator
-                return self.calc_next_part_2_edge();
-            }
-            // If bottom left corner is track, we're done.
-            else {
-                println!("Part 2: {:?} {}", self.last_re, (self.last_re.x - (box_size - 1)) * 10000 + self.last_le.y);
-                self.part_2_done = true;
-                return test_pos;
-            }
+            println!("Part 2: {}", 
+                    (self.last_re.x - (box_size - 1)) * 10000 + self.last_le.y);
+            self.part_2_done = true;
+            return test_pos;
         }
     }
 }
@@ -283,13 +244,11 @@ fn main() {
     let input_prog: Vec<i64> = (&read_ints_comma_sep(&filename)[0]).to_vec();
 
     let mut drone = Drone::new();
-    // /*
     while !drone.input_done {
         run_program(0, &input_prog, &mut drone);
     }
     drone.print_map(0, 49, 0, 49);
     println!("Part 1: {}", drone.trac_pt_cnt);
-    //*/
 
     drone.part_1 = false;
     while !drone.part_2_done {
