80beff80ac20725477252da1f528bc4a8f504563
Karl Sickendick <kc0bfv@gmail.com>
2025-01-02 23:18:36 +0000

    Part 2 solved now

--- 2024/16/main.py
+++ 2024/16/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from collections import defaultdict, namedtuple
+from itertools import chain
 from enum import Enum
 import sys
 
@@ -30,87 +31,75 @@ def add_pts(a, b):
     return Point(a.x + b.x, a.y + b.y)
 
 def list_moves(lines, pos):
-    deltas = [
-        (Pos(pos.dir.fwd_delta(), pos.dir), 1),
-        (Pos(pos.dir.rot_cw().fwd_delta(), pos.dir.rot_cw()), 1001),
-        (Pos(pos.dir.rot_ccw().fwd_delta(), pos.dir.rot_ccw()), 1001),
-        (Pos(pos.dir.rot_180().fwd_delta(), pos.dir.rot_180()), 2001),
+    moves_a = [
+        (Pos(add_pts(pos.pt, pos.dir.fwd_delta()), pos.dir), 1),
+        (Pos(pos.pt, pos.dir.rot_cw()), 1000),
+        (Pos(pos.pt, pos.dir.rot_ccw()), 1000),
+        (Pos(pos.pt, pos.dir.rot_180()), 2000),
     ]
-    moves_a = ((Pos(add_pts(pos.pt, delt.pt), delt.dir), sc) for delt, sc in deltas)
     moves_b = ((pos, sc) for pos, sc in moves_a
         if 0 <= pos.pt.x < len(lines[0]) and 0 <= pos.pt.y < len(lines)
     )
     moves = [(pos, sc) for pos, sc in moves_b
-        if lines[pos.pt.y][pos.pt.x] in [".", "E"]
+        if lines[pos.pt.y][pos.pt.x] in ["S",".", "E"]
     ]
     return moves
 
-def list_moves_backward(lines, pos):
-    def fix_cost(cost):
-        return {1: 2001, 2001: 1, 1001: 1001}[cost]
-    return [(Pos(pos.pt, pos.dir.rot_180()), fix_cost(cost))
-        for pos, cost in list_moves(lines, pos)]
-
 def walk_all(lines, st_pos):
     cheapest_costs = dict()
-    cheapest_costs[st_pos.pt] = 0
+    cheapest_costs[st_pos] = 0
     cheapest_ways = dict()
-    cheapest_ways[st_pos.pt] = list([st_pos])
+    cheapest_ways[st_pos] = list()
 
     frontier = set([(st_pos, 0)])
     while len(frontier) > 0:
+        #print(frontier)
         new_frontier = set()
         for front_pos, cur_cost in frontier:
+            #print(front_pos, list_moves(lines, front_pos))
             for move, cost in list_moves(lines, front_pos):
-                if move.pt.x == 5 and move.pt.y == 7:
-                    print(move, front_pos, cost, cur_cost + cost)
-                if move.pt not in cheapest_costs or \
-                    cheapest_costs[move.pt] >= cost + cur_cost:
-                    if move.pt not in cheapest_costs or \
-                        cheapest_costs[move.pt] > cost + cur_cost:
-                        cheapest_ways[move.pt] = list()
+                #if move.pt.x == 5 and move.pt.y == 7:
+                #    print(move, front_pos, cost, cur_cost + cost)
 
-                    cheapest_costs[move.pt] = cost + cur_cost
-                    cheapest_ways[move.pt].append(move)
+                if move not in cheapest_costs or \
+                    cheapest_costs[move] >= cost + cur_cost:
+                    if move not in cheapest_costs or \
+                        cheapest_costs[move] > cost + cur_cost:
+                        cheapest_ways[move] = list()
+
+                    cheapest_costs[move] = cost + cur_cost
+                    cheapest_ways[move].append(front_pos)
                     new_frontier.add((move, cost + cur_cost))
         frontier = new_frontier
 
     return cheapest_costs, cheapest_ways
 
 def get_paths(lines, cheapest_costs, cheapest_ways, ed_pt, st_pt):
-    on_path = set()
-
-    frontier = set(cheapest_ways[ed_pt])
+    all_ed_pos = [Pos(ed_pt, dir) for dir in Direction]
+    ed_pos_costs = [cheapest_costs[ed_pos] for ed_pos in all_ed_pos]
+    cheapest = min(cheapest_costs[ed_pos] for ed_pos in all_ed_pos)
+    cheapest_ends = [ed_pos
+        for cost, ed_pos in zip(ed_pos_costs, all_ed_pos)
+        if cost == cheapest]
+    valid_ed_pos = [cheapest_ways[ed_pos]
+        for ed_pos in cheapest_ends
+        if ed_pos in cheapest_ways]
+    
+    on_path = set([ed_pt])
+    frontier = set(chain.from_iterable(valid_ed_pos))
     on_path.update(way.pt for way in frontier)
-    print(frontier, on_path)
+
+    #print(frontier, on_path)
     while len(frontier) > 0:
         new_frontier = set()
         for front_pos in frontier:
-            front_cost = cheapest_costs[front_pos.pt]
+            front_cost = cheapest_costs[front_pos]
+            # all the cheapest prior positions to get here for front_cost
+            ways = cheapest_ways[front_pos]
+            #print("Ways", ways)
 
-            # Check each of the nearby positions we could have come from
-            poss_delts = [(1,0), (-1, 0), (0, 1), (0, -1)]
-            for delt in poss_delts:
-                nearby_pt = add_pts(front_pos.pt, Point(*delt))
-                if nearby_pt not in cheapest_costs:
-                    continue
-                if cheapest_costs[nearby_pt] >= front_cost:
-                    continue
-
-                # Get all of the cheapest ways to get to the nearby_pt
-                nearby_pt_ways = cheapest_ways[nearby_pt]
-                #print(nearby_pt, nearby_pt_ways)
-                
-                # For each of those nearby points look at possible moves
-                # and see if our frontier is one of them
-                for nearby_pt_way in nearby_pt_ways:
-                    moves = list_moves(lines, nearby_pt_way)
-                    for move, cost in moves:
-                        if move.pt == front_pos.pt and \
-                            move.dir == front_pos.dir and \
-                            cheapest_costs[nearby_pt] + cost == front_cost:
-                            on_path.add(nearby_pt)
-                            new_frontier.add(nearby_pt_way)
+            new_frontier.update(ways)
+            on_path.update(way.pt for way in ways)
                         
         #print("NEW ", new_frontier)
         frontier = new_frontier
@@ -134,9 +123,12 @@ def main(filename):
     ed_pt = Point(ed_pt_r[0], ed_pt_r[1])
 
     cheapest_costs, cheapest_ways = walk_all(lines, Pos(st_pt, Direction.EAST))
-    part1 = cheapest_costs[ed_pt]
-    print(f"Part 1: {part1}")
-
+    #print(cheapest_costs)
+    all_ed_pos = [Pos(ed_pt, dir) for dir in Direction]
+    part1_l = [cheapest_costs[ed_pos] for ed_pos in all_ed_pos if ed_pos in cheapest_costs]
+    #print(part1_l)
+    print(f"Part 1: {min(part1_l)}")
+    
     path = get_paths(lines, cheapest_costs, cheapest_ways, ed_pt, st_pt)
     print(draw_map(lines, path))
     print(f"Part 2: {len(path)}")
